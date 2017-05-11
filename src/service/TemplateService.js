7a08e077e51cd9d20f4e0ac8eb6a212f54cc1434
ffjanhoeck <jan.hoeck@flowfact.de>
2017-05-11 13:38:35 +0200

    Remove headers in template service.

--- src/service/TemplateService.js
+++ src/service/TemplateService.js
@@ -22,11 +22,7 @@ export default class TemplateService {
         const formData = new FormData();
         formData.append('file', file, file.name);
 
-        return this.client.makeRequest({}, `/templates/${id}/content`, 'PUT', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
+        return this.client.makeRequest({}, `/templates/${id}/content`, 'POST', formData)
     }
 
     getTemplateById(id) {
