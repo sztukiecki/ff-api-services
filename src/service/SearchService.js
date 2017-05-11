9dc73dae7125e5cb0aaa4633996c4fcbc98724a3
Fabian Dreßen <dev-fdr@flowfact.com>
2017-05-11 10:59:18 +0200

    adapted search service

--- src/service/SearchService.js
+++ src/service/SearchService.js
@@ -7,10 +7,38 @@ export default class SearchService {
         this.client = new HttpClient(APIMapping.searchService);
     }
 
-    search(query, index) {
+    search(query, index, page = 1, size = null) {
         if (typeof query === 'string') {
             query = JSON.parse(query);
         }
         return this.client.makeRequetSimple(query, '/index/' + index, 'POST');
+    }
+
+
+    filter(index, page = 1, size = null, filter) {
+        return this.client.makeRequest({}, '/index/' + index, 'POST', this.getQuery(filter), {
+            queryParams: {
+                page: page,
+                size: size
+            }
+        });
+    }
+
+    getQuery(filter) {
+        if (!filter) {
+            return {
+                query: {
+                    match_all: {}
+                }
+            }
+        } else {
+            return {
+                query: {
+                    match: {
+                        _all: filter
+                    }
+                }
+            }
+        }
     }
 }
