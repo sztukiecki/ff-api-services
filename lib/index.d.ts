c695d4fe22ef6045262eb5369d9868a566e0a7c0
Daniel Fischer <daniel.fischer@flowfact.de>
2017-06-13 09:38:03 +0200

    define email service response format

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,22 @@
 declare module 'ff-api-services' {
-    export class EmailService {
-        static createDomain(domain): Promise<any>;
-        static verifyDomain(domain): Promise<any>;
+    interface EmailServiceVerifyDnsEntry {
+        valid: boolean;
+        type: "a" | "cname";
+        host: string;
+        data: string;
     }
+
+    interface EmailServiceVerifyResponse {
+        domain: string;
+        valid: boolean;
+        dnsEntries: EmailServiceVerifyDnsEntry[];
+    }
+
+    export class EmailService {
+        static createDomain(domain): Promise<EmailServiceVerifyResponse>;
+        static verifyDomain(domain): Promise<EmailServiceVerifyResponse>;
+    }
+
     export class ViewDefinitionService {
         static getDefinitionsForSchema(schemaId): Promise<any>
     }
