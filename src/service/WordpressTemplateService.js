ff96b897c5890dd7155af85e01c57fc8858a8142
ffphilippmaurer <philipp.maurer@flowfact.de>
2017-07-19 17:46:41 +0200

    Add duplicate page ressource to wordpress api.

--- src/service/WordpressTemplateService.js
+++ src/service/WordpressTemplateService.js
@@ -68,6 +68,30 @@ export default class WordpressTemplateService {
     }
 
     /**
+     * Duplicate an existing wordpress page.
+     * A wordpress template is directly connected to the template service. Any reference to "template",
+     * like "template id", means the corresponding attribute from the <b>template service</b>.
+     *
+     * A wordpress template, without a corresponding template from the template service, can not be rendered or manipulated.
+     *
+     * @param companyId         The company, to create the page for.
+     * @param oldTemplateId     The id of the template, which shall be duplicated
+     * @param newTemplateId     The id of the newly created template.
+     * @returns {*}
+     */
+    static duplicatePage(companyId, oldTemplateId, newTemplateId) {
+        if (this.cognitoToken) {
+            const wordpressApi = this.getWordpressApi(companyId);
+            return wordpressApi
+                .duplicatePage()
+                .param('cognitoToken', this.cognitoToken)
+                .param('oldTemplateId', oldTemplateId)
+                .param('newTemplateId', newTemplateId);
+        }
+        return false;
+    }
+
+    /**
      * Delete a site with a specific name.
      *
      * @param companyId 	The company of the current user scope, which is also the blog name for the wordpress api
@@ -77,9 +101,9 @@ export default class WordpressTemplateService {
     static deletePage(companyId, templateId) {
         if (this.cognitoToken) {
             const wordpressApi = this.getWordpressApi(companyId);
-			return wordpressApi.pages().slug(templateId).param('cognitoToken', this.cognitoToken).then(page => {
-				if (page.length > 0) {
-				    page = page.shift();
+			return wordpressApi.pages().slug(templateId).param('cognitoToken', this.cognitoToken).then(pages => {
+				if (pages.length > 0) {
+				    const page = pages.shift();
 					return wordpressApi.pages().id(page.id).param('cognitoToken', this.cognitoToken).delete();
 				}
 				return false;
@@ -101,6 +125,7 @@ export default class WordpressTemplateService {
             this.wordpressApis[companyId] = new WPAPI({
                 endpoint: `${this.wordpressUrl}${companyId}/index.php/wp-json`
             });
+            this.wordpressApis[companyId].duplicatePage = this.wordpressApis[companyId].registerRoute('wp/v2', '/pages/duplicate');
         }
 
         return this.wordpressApis[companyId];
