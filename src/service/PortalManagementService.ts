799b7d0e81c95b773107f2b68412f44ff59d0dd4
Jan Höck <dev-jho@flowfact.com>
2018-08-09 12:33:42 +0200

    More ressource calls for the portal management service

--- src/service/PortalManagementService.ts
+++ src/service/PortalManagementService.ts
@@ -7,10 +7,23 @@ export class PortalManagementService extends APIClient {
         super(APIMapping.portalManagementService);
     }
 
+    fetchPortals(): Promise<AxiosResponse> {
+        return this.invokeApi('/portals', 'GET');
+    }
+
+    fetchPortal(portalId: string): Promise<AxiosResponse> {
+        return this.invokeApi(`/portals/${portalId}`, 'GET');
+    }
+
     fetchPortalTypes(): Promise<AxiosResponse> {
         return this.invokeApi('/portalTypes', 'GET');
     }
 
+    registerPortal(portalType: 'IS24' | 'OPEN_IMMO'): Promise<AxiosResponse> {
+        return this.invokeApi('/portals/register', 'POST', {
+            portalType: portalType
+        });
+    }
 }
 
 export default new PortalManagementService();
