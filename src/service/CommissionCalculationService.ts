eb8354dcab48f1ee4031b2db0a84ab282e5d3890
Daniel Fischer <daniel.fischer@flowfact.de>
2018-08-31 09:38:08 +0200

    add separate commission & some typings

--- src/service/CommissionCalculationService.ts
+++ src/service/CommissionCalculationService.ts
@@ -1,14 +1,27 @@
 import {APIClient, APIMapping} from '../http';
 import { AxiosResponse } from 'axios';
 
+export interface TotalCommission {
+	totalCommission: number;
+}
+
+export interface CommissionEntry {
+	entityId: string;
+	commission: number;
+}
+
 export class CommissionCalculationService extends APIClient {
 	constructor() {
 		super(APIMapping.commissionCalculationService);
 	}
 
-	calculateTotalCommission(entityIds: Array<string>): Promise<AxiosResponse> {
-		return this.invokeApi(`/calculateTotalCommission`, 'POST', entityIds);
-	}
+    calculateTotalCommission(entityIds: string[]): Promise<AxiosResponse<TotalCommission>> {
+        return this.invokeApi(`/calculateTotalCommission`, 'POST', entityIds);
+    }
+
+    calculateSeparateCommission(entityIds: string[]): Promise<AxiosResponse<CommissionEntry[]>> {
+        return this.invokeApi(`/calculateSeparateCommission`, 'POST', entityIds);
+    }
 }
 
 export default new CommissionCalculationService();
