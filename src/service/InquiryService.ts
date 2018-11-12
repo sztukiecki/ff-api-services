c15b4043bd0a51348cee1ab0a3e90e14c544be67
Gregor Müller <Gregor.Mueller@flowfact.de>
2018-11-12 17:19:46 +0100

    add patch resource for inquiry service

--- src/service/InquiryService.ts
+++ src/service/InquiryService.ts
@@ -11,6 +11,10 @@ export class InquiryService extends APIClient {
 	getAll(page: number = 1, size: number = 100): Promise<AxiosResponse<Array<Inquiry>>> {
 		return this.invokeApi(`/inquiry?page=${page}&size=${size}`, 'GET');
 	}
+
+	updateInquiries(inquiries: Array<Inquiry>): Promise<AxiosResponse> {
+		return this.invokeApi(`/inquiry`, 'PATCH', inquiries);
+	}
 }
 
 export default new InquiryService();
