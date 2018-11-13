672de99632705c06ccc497dddce98544eb538384
Andreas Soroko <andreas.soroko@flowfact.de>
2018-11-13 11:33:25 +0100

    [Matchmaking] forgot to add page param

--- src/service/MatchmakingService.ts
+++ src/service/MatchmakingService.ts
@@ -10,16 +10,22 @@ export class MatchmakingService extends APIClient {
     }
 
 
-    getAllMatches(): Promise<AxiosResponse<MatchmakingPagingResponse<Array<Contact>>>> {
-        return this.invokeApi('/matches', 'GET');
+    getAllMatches(page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<Contact>>>> {
+        return this.invokeApi('/matches', 'GET', undefined, {
+            queryParams: { page }
+        });
     }
 
-    getMatchesByContact(contactId: string): Promise<AxiosResponse<MatchmakingPagingResponse<Array<MatchScore>>>> {
-        return this.invokeApi('/matches/contact/' + contactId, 'GET');
+    getMatchesByContact(contactId: string, page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<MatchScore>>>> {
+        return this.invokeApi(`/matches/contact/${contactId}`, 'GET', undefined, {
+            queryParams: { page }
+        });
     }
 
-    getMatchesByEstate(estateId: string): Promise<AxiosResponse<MatchmakingPagingResponse<Array<MatchScore>>>> {
-        return this.invokeApi('/matches/estate/' + estateId, 'GET');
+    getMatchesByEstate(estateId: string, page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<MatchScore>>>> {
+        return this.invokeApi(`/matches/estate/${estateId}`, 'GET', undefined, {
+            queryParams: { page }
+        });
     }
 
     getMatchCountByEstate(estateId: string): Promise<AxiosResponse<MatchCountForEstate>> {
