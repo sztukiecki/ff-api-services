e76f04009dd765efd61643051117b93f34fd3a78
Fabian Dreßen <dev-fdr@flowfact.com>
2018-11-20 21:44:03 +0100

    Added create new Subscription + upgrade account

--- src/service/TrialLicenseService.ts
+++ src/service/TrialLicenseService.ts
@@ -14,6 +14,14 @@ export class TrialLicenseService extends APIClient {
     getCustomerDataByEmail(submissionGuid: string) {
         return this.invokeApi(`/public/freeTrial/email/${submissionGuid}`, 'GET').then(s => s.data);
     }
+
+    createNewSubscription(payload: any) {
+        return this.invokeApi('/public/buy', 'POST', payload).then(s => s.data);
+    }
+
+    upgradeAccount() {
+        return this.invokeApi('/upgradeAccount', 'POST').then(s => s.data);
+    }
 }
 
 export default new TrialLicenseService();
