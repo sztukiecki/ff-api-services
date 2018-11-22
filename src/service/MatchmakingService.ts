7fbd25a42763e99deaddfa1646435c88d180cf6c
Gregor Müller <Gregor.Mueller@flowfact.de>
2018-11-22 16:43:39 +0100

    update MatchmakingService

--- src/service/MatchmakingService.ts
+++ src/service/MatchmakingService.ts
@@ -5,36 +5,46 @@ import { MatchCountForEstate, MatchmakingPagingResponse, MatchScore } from '../u
 
 export class MatchmakingService extends APIClient {
 
-    constructor() {
-        super(APIMapping.matchmakingService);
-    }
+	constructor() {
+		super(APIMapping.matchmakingService);
+	}
 
+	getAllMatches(page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<Contact>>>> {
+		console.warn('getAllMatches() is deprecated! Use getAllMatchesForContacts() instead!');
+		return this.getAllMatchesForContacts(page);
+	}
 
-    getAllMatches(page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<Contact>>>> {
-        return this.invokeApi('/matches', 'GET', undefined, {
-            queryParams: { page }
-        });
-    }
+	getAllMatchesForContacts(page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<Contact>>>> {
+		return this.invokeApi('/matches/contacts', 'GET', undefined, {
+			queryParams: { page }
+		});
+	}
 
-    getMatchesByContact(contactId: string, page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<MatchScore>>>> {
-        return this.invokeApi(`/matches/contact/${contactId}`, 'GET', undefined, {
-            queryParams: { page }
-        });
-    }
+	getAllMatchesForEstates(page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<Contact>>>> {
+		return this.invokeApi('/matches/estates', 'GET', undefined, {
+			queryParams: { page }
+		});
+	}
 
-    getMatchesByEstate(estateId: string, page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<MatchScore>>>> {
-        return this.invokeApi(`/matches/estate/${estateId}`, 'GET', undefined, {
-            queryParams: { page }
-        });
-    }
+	getMatchesByContact(contactId: string, page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<MatchScore>>>> {
+		return this.invokeApi(`/matches/contacts/${contactId}`, 'GET', undefined, {
+			queryParams: { page }
+		});
+	}
 
-    getMatchCountByEstate(estateId: string): Promise<AxiosResponse<MatchCountForEstate>> {
-        return this.invokeApi(`/matches/estate/${estateId}/count`, 'GET');
-    }
+	getMatchesByEstate(estateId: string, page: number = 0): Promise<AxiosResponse<MatchmakingPagingResponse<Array<MatchScore>>>> {
+		return this.invokeApi(`/matches/estates/${estateId}`, 'GET', undefined, {
+			queryParams: { page }
+		});
+	}
 
-    initialImport(): Promise<AxiosResponse> {
-        return this.invokeApi('/trigger/initial', 'POST');
-    }
+	getMatchCountByEstate(estateId: string): Promise<AxiosResponse<MatchCountForEstate>> {
+		return this.invokeApi(`/matches/estates/${estateId}/count`, 'GET');
+	}
+
+	initialImport(): Promise<AxiosResponse> {
+		return this.invokeApi('/trigger/initial', 'POST');
+	}
 }
 
 export default new MatchmakingService();
