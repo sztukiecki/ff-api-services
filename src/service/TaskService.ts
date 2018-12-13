1b3f8e2513cde810940bec359a08e56fbc28205f
Gregor Müller <Gregor.Mueller@flowfact.de>
2018-12-13 10:55:10 +0100

    update task endpoints

--- src/service/TaskService.ts
+++ src/service/TaskService.ts
@@ -6,12 +6,24 @@ export class TaskService extends APIClient {
 		super(APIMapping.taskService);
 	}
 
-	getAll() {
-		return this.invokeApi('/tasks', 'GET').then(s => s.data);
+	getById(id: string) {
+		return this.invokeApi(`/task/${id}`, 'GET').then(s => s.data);
 	}
 
-	getById(id: string) {
-		return this.invokeApi(`/tasks/${id}`, 'GET').then(s => s.data);
+	getTaskForEntityById(entityId: string) {
+		return this.invokeApi(`/task/forEntity/${entityId}`, 'GET').then(s => s.data);
+	}
+
+	deleteTask(id: string) {
+		return this.invokeApi(`/task/${id}`, 'DELETE').then(s => s.data);
+	}
+
+	createTask(task: object) {
+		return this.invokeApi(`/task`, 'POST', task).then(s => s.data);
+	}
+
+	updateTask(id: string, task: object) {
+		return this.invokeApi(`/task/`, 'PUT', task).then(s => s.data);
 	}
 
 }
