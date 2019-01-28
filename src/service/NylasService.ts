b2c52f710986f88138e15ab904d2392b7b9b38d2
Stefan Hering <stefan.hering@flowfact.de>
2019-01-28 11:29:27 +0100

    FLOW-585 endpoint for sending nylas emails

--- src/service/NylasService.ts
+++ src/service/NylasService.ts
@@ -2,6 +2,25 @@ import APIClient from '../http/APIClient';
 import APIMapping from '../http/APIMapping';
 import { AxiosResponse } from 'axios';
 
+interface SendEmailRequest {
+	subject: string;
+	to: string[];
+	cc: string[];
+	bcc: string[];
+	from: string;
+	reply_to: string[];
+	body: string;
+	file_ids: string[];
+	tracking: Tracking;
+}
+
+interface Tracking {
+	links: boolean;
+	opens: boolean;
+	thread_replies: boolean;
+	payload: string;
+}
+
 export class NylasService extends APIClient {
 
     constructor() {
@@ -16,6 +35,13 @@ export class NylasService extends APIClient {
         });
     }
 
+	async sendMail(emailAccount: string, email: SendEmailRequest): Promise<AxiosResponse> {
+		return await this.invokeApi('/nylas/send', 'POST', email, {
+			'queryParams': {
+				'email': emailAccount
+			}
+		});
+	}
 }
 
 export default new NylasService();
