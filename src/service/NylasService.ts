05ac2fde33e16c4a237fceaf24ba704e6638215e
Stefan Hering <stefan.hering@flowfact.de>
2019-01-28 15:57:48 +0100

    add config endpoint

--- src/service/NylasService.ts
+++ src/service/NylasService.ts
@@ -3,27 +3,31 @@ import APIMapping from '../http/APIMapping';
 import { AxiosResponse } from 'axios';
 
 interface SendEmailRequest {
-	subject: string;
-	to: EmailAddress[];
-	cc: EmailAddress[];
-	bcc: EmailAddress[];
-	from: EmailAddress[];
-	reply_to: EmailAddress[];
-	body: string;
-	file_ids: string[];
-	tracking: Tracking;
+    subject: string;
+    to: EmailAddress[];
+    cc: EmailAddress[];
+    bcc: EmailAddress[];
+    from: EmailAddress[];
+    reply_to: EmailAddress[];
+    body: string;
+    file_ids: string[];
+    tracking: Tracking;
 }
 
 interface EmailAddress {
-	email: string;
-	name: string;
+    email: string;
+    name: string;
 }
 
 interface Tracking {
-	links: boolean;
-	opens: boolean;
-	thread_replies: boolean;
-	payload: string;
+    links: boolean;
+    opens: boolean;
+    thread_replies: boolean;
+    payload: string;
+}
+
+interface NylasConfig {
+    emails: EmailAddress[];
 }
 
 export class NylasService extends APIClient {
@@ -40,13 +44,17 @@ export class NylasService extends APIClient {
         });
     }
 
-	async sendMail(emailAccount: string, email: SendEmailRequest): Promise<AxiosResponse> {
-		return await this.invokeApi('/nylas/send', 'POST', email, {
-			'queryParams': {
-				'email': emailAccount
-			}
-		});
-	}
+    async sendMail(emailAccount: string, email: SendEmailRequest): Promise<AxiosResponse> {
+        return await this.invokeApi('/nylas/send', 'POST', email, {
+            'queryParams': {
+                'email': emailAccount
+            }
+        });
+    }
+
+    async getConfig(): Promise<AxiosResponse<NylasConfig>> {
+        return await this.invokeApi('config', 'GET');
+    }
 }
 
 export default new NylasService();
