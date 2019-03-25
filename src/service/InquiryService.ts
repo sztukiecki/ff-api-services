adae7809378881922cbf166326595825d2fb9c10
Gregor Müller <Gregor.Mueller@flowfact.de>
2019-03-25 14:07:01 +0100

    setEstate endpoint in inquiry service now returns the updated inquiry

--- src/service/InquiryService.ts
+++ src/service/InquiryService.ts
@@ -9,20 +9,21 @@ export class InquiryService extends APIClient {
     }
 
     /**
-     * TODO: Please comment this method
-     * @param page
-     * @param size
+     * Fetches all inquiries with pagination support
+     * @param {number} page - Number of times the result will be offset by given size.
+     * @param {number} size - Number of entities to fetch.
      */
     async fetchAll(page: number = 1, size: number = 100): Promise<AxiosResponse<Array<Inquiry>>> {
         return await this.invokeApi(`/inquiry?page=${page}&size=${size}`, 'GET');
     }
 
     /**
-     * TODO: Please comment this method
-     * @param inquiryId
-     * @param estateId
+     * Use this method to link an estate with given ID to an inquiry with given ID, that has no estate linked yet.
+     * @param {string} inquiryId - ID of the inquiry that will be updated.
+     * @param {string} estateId - ID of the estate that will be linked to the inquiry.
+     * @returns the updated inquiry
      */
-    async linkEstateAndStartAutomation(inquiryId: string, estateId: string): Promise<AxiosResponse> {
+    async linkEstateAndStartAutomation(inquiryId: string, estateId: string): Promise<AxiosResponse<Inquiry>> {
         return await this.invokeApi(`/inquiry/${inquiryId}/setEstate/${estateId}`, 'POST');
     }
 
