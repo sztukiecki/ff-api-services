5ad10c78573066d1f352c07bb8434b486d587151
Gregor Müller <Gregor.Mueller@flowfact.de>
2019-06-05 09:28:57 +0200

    update OnboardingService and types

--- src/service/OnboardingService.ts
+++ src/service/OnboardingService.ts
@@ -1,61 +1,6 @@
-import { APIClient, APIMapping } from '../http';
+import { ZohoAddon, ZohoPlan } from '@flowfact/types';
 import { AxiosResponse } from 'axios';
-
-interface ZohoPlan {
-    planCode: string;
-    name: string;
-    description: string;
-    setupFee: number;
-    recurringPrice: number;
-    interval: number;
-    unit: string;
-    storeMarkupDescription: string;
-    status: string;
-    productId: string;
-    accountId: string;
-    accountName: string;
-    trialPeriod: number;
-    setupFeeAccountId: string;
-    setupFeeAccountName: string;
-    tags: any;
-    customFields: any;
-    billingCycles: number;
-    url: string;
-    taxId: string;
-    productType: string;
-    hsnOrSac: string;
-    createdTime: string;
-    updatedTime: string;
-}
-
-export interface PriceBracket {
-    price: number;
-}
-
-export interface ZohoAddon {
-    name: string;
-    addonCode: string;
-    description: string;
-    status: string;
-    unitName: string;
-    productId: string;
-    taxId: string;
-    taxName: string;
-    taxPercentage: number;
-    taxType: string;
-    applicableToAllPlans: boolean;
-    productType: string;
-    showInWidget: boolean;
-    storeDescription: string;
-    storeMarkupDescription: string;
-    type: string;
-    intervalUnit: string;
-    createdTime: Date;
-    updatedTime: Date;
-    pricingScheme: string;
-    plans: any[];
-    priceBrackets: PriceBracket[];
-}
+import { APIClient, APIMapping } from '../http';
 
 export class OnboardingService extends APIClient {
     constructor() {
@@ -67,7 +12,7 @@ export class OnboardingService extends APIClient {
      * @returns {Promise<AxiosResponse>}
      */
     async onboardCurrentUser(): Promise<AxiosResponse> {
-        return await this.invokeApi('/customer', 'POST');
+        return this.invokeApi('/customer', 'POST');
     }
 
     /**
@@ -78,10 +23,10 @@ export class OnboardingService extends APIClient {
     async setupAccount(bundleName: string, withEntities: boolean = true): Promise<AxiosResponse> {
         const queryParams: any = {
             bundleName: bundleName,
-            withEntities: withEntities.toString()
+            withEntities: withEntities.toString(),
         };
 
-        return await this.invokeApi('/customer/setup', 'POST', undefined, {queryParams});
+        return this.invokeApi('/customer/setup', 'POST', undefined, { queryParams });
     }
 
     /**
@@ -90,23 +35,22 @@ export class OnboardingService extends APIClient {
      */
     async createNewSubscription(numberOfUsers: string) {
         const queryParams: any = {
-            numberOfUsers: numberOfUsers
+            numberOfUsers: numberOfUsers,
         };
-        return await this.invokeApi('/buypage/generateUrl', 'POST', undefined, {queryParams});
+        return this.invokeApi('/buypage/generateUrl', 'POST', undefined, { queryParams });
     }
-
 
     /**
      * Fetch the default zoho plan, to get current information on pricing
      */
-    async fetchSubscriptionPlan(): Promise<AxiosResponse<ZohoPlan>>  {
+    async fetchSubscriptionPlan(): Promise<AxiosResponse<ZohoPlan>> {
         return this.invokeApi('/plan', 'GET');
     }
 
     /**
      * Fetch the default zoho additional user addon
      */
-    async fetchAddon(): Promise<AxiosResponse<ZohoAddon>>  {
+    async fetchAddon(): Promise<AxiosResponse<ZohoAddon>> {
         return this.invokeApi('/addon', 'GET');
     }
 
@@ -123,21 +67,21 @@ export class OnboardingService extends APIClient {
      * @param answer
      */
     async answerQuestion(questionId: number, answer: any) {
-        return await this.invokeApi('/qualifications/answer/' + questionId, 'POST', {answer});
+        return this.invokeApi('/qualifications/answer/' + questionId, 'POST', { answer });
     }
 
     /**
      * Tells backend, that the "Buy Now" button was clicked.
      */
     async trackBuyNowClicked() {
-        return await this.invokeApi('/track/buynowclicked', 'POST');
+        return this.invokeApi('/track/buynowclicked', 'POST');
     }
 
     /**
      * Tells backend, that the terms where confirmed. (For example: After click onto the "order now" button)
      */
     async trackTermsConfirmed() {
-        return await this.invokeApi('/track/termsconfirmed', 'POST')
+        return this.invokeApi('/track/termsconfirmed', 'POST');
     }
 }
 
