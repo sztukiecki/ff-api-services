7981e9a24022de7d546e7d7606ebcd266e72f6b3
ffjanhoeck <jan.hoeck@flowfact.de>
2019-07-18 15:11:36 +0200

    add fetch workflow method

--- src/service/WorkflowService.ts
+++ src/service/WorkflowService.ts
@@ -29,6 +29,15 @@ class WorkflowService extends APIClient {
     fetchTemplates = async(): Promise<AxiosResponse> => {
         return await this.invokeApi('/flow-type/templates', 'GET');
     };
+
+    /**
+     * Fetches a specific workflow
+     * @param workflowId
+     *  The id of the workflow
+     */
+    fetchWorkflow = async(workflowId: string) => {
+        return await this.invokeApi(`/flow/${workflowId}`, 'GET');
+    }
 }
 
 export default new WorkflowService();
