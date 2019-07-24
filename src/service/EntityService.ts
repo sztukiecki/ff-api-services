eff4bd7004e5779e38640e9512693863a8c7c159
Hakan Gündüz <hakan.guenduez@flowfact.de>
2019-07-24 15:41:26 +0200

    Add optional param "withCount" to EntityService

--- src/service/EntityService.ts
+++ src/service/EntityService.ts
@@ -58,12 +58,14 @@ export class EntityService extends APIClient {
      * @param flowdsl
      * @param page
      * @param size
+     * @param withCount
      */
-    async searchEntity(index: string, viewName: string, flowdsl?: Flowdsl, page: number = 1, size: number = 20) {
+    async searchEntity(index: string, viewName: string, flowdsl?: Flowdsl, page: number = 1, size: number = 20, withCount?: string) {
         const queryParams: ParamList = {
             page,
             size,
             viewName,
+            withCount
         };
 
         return this.invokeApi<SearchResult<EntityView>>(`/search/schemas/${index}`, 'POST', flowdsl, {
@@ -78,12 +80,14 @@ export class EntityService extends APIClient {
      * @param flowdsl
      * @param offset
      * @param size
+     * @param withCount
      */
-    async fetchEntitiesVirtualized(index: string, viewName: string, flowdsl?: Flowdsl, offset: number = 0, size: number = 20) {
+    async fetchEntitiesVirtualized(index: string, viewName: string, flowdsl?: Flowdsl, offset: number = 0, size: number = 20, withCount?: string) {
         const queryParams: ParamList = {
             offset,
             size,
             viewName,
+            withCount
         };
 
         return this.invokeApi<SearchResult<EntityView>>(`/search/schemas/${index}`, 'POST', flowdsl, {
@@ -98,12 +102,14 @@ export class EntityService extends APIClient {
      * @param flowdsl
      * @param offset
      * @param size
+     * @param withCount
      */
-    async fetchEntitiesVirtualizedV2(index: string, viewName: string, flowdsl?: Flowdsl, offset: number = 0, size: number = 20) {
+    async fetchEntitiesVirtualizedV2(index: string, viewName: string, flowdsl?: Flowdsl, offset: number = 0, size: number = 20, withCount?: string) {
         const queryParams: ParamList = {
             offset,
             size,
             viewName,
+            withCount
         };
 
         return this.invokeApi<SearchResult<EntityView>>(`/search/schemas/${index}`, 'POST', flowdsl, {
