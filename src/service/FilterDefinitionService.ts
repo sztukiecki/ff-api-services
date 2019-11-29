944c6524bb8d79511b4fd40eae08581f7abbbae7
ffjanhoeck <jan.hoeck@flowfact.de>
2019-11-29 07:01:38 +0100

    [FilterDefinitionService] add ressources

--- src/service/FilterDefinitionService.ts
+++ src/service/FilterDefinitionService.ts
@@ -1,4 +1,6 @@
 import { APIClient, APIMapping } from '../http';
+import { AxiosResponse } from 'axios';
+import { FilterDefinition } from '@flowfact/types';
 
 class FilterDefinitionService extends APIClient {
 
@@ -10,12 +12,37 @@ class FilterDefinitionService extends APIClient {
      * Fetchs filter for a specific schema
      * @param schemaName
      */
-    fetchFilter = async (schemaName: string) => {
+    async fetchFilter(schemaName: string): Promise<AxiosResponse<FilterDefinition>> {
         return await this.invokeApi('/filters', 'GET', undefined, {
             queryParams: {
                 schemaName: schemaName
             }
         });
+    }
+
+    /**
+     * Get all saved filters for a schema
+     * @param schemaName
+     */
+    async fetchSavedFilters(schemaName: string): Promise<AxiosResponse<FilterDefinition[]>> {
+        return await this.invokeApi('/filters/saved', 'GET');
+    }
+
+    /**
+     * Create a new saved filter for a schema
+     * @param filterDefinition
+     */
+    async createSavedFilter(filterDefinition: FilterDefinition): Promise<AxiosResponse<FilterDefinition>> {
+        return await this.invokeApi('/filters/saved', 'POST', filterDefinition);
+    }
+
+    /**
+     * Update a saved filter by his id.
+     * @param filterId
+     * @param filterDefinition
+     */
+    async updateSavedFilter(filterId: string, filterDefinition: FilterDefinition): Promise<AxiosResponse<FilterDefinition>> {
+        return await this.invokeApi(`/filters/saved/${filterId}`, 'PATCH', filterDefinition);
     }
 }
 
