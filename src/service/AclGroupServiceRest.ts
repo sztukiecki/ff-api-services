81d700a2c07268373fda58a3141174df15d6f7e1
Hakan Gündüz <47702289+ffhakanguenduez@users.noreply.github.com>
2020-01-28 16:06:49 +0100

    Flow 4808 (#33)
    FLOW-4808: Add endpoints for acp template management

--- src/service/AclGroupServiceRest.ts
+++ src/service/AclGroupServiceRest.ts
@@ -1,5 +1,6 @@
+import { AxiosResponse } from 'axios';
 import { APIClient, APIMapping } from '../http';
-import { AclGroup, AclGroupType } from '@flowfact/types';
+import { AclGroup, AclGroupType, AcpTemplate } from '@flowfact/types';
 
 class AclGroupServiceRest extends APIClient {
 
@@ -42,6 +43,55 @@ class AclGroupServiceRest extends APIClient {
     async deleteGroup(group: AclGroup) {
         return this.invokeApi(`/groups/${group.id}`, 'DELETE', group);
     }
+
+    /**
+     * Fetch available templates
+     */
+    async fetchAcpTemplates(): Promise<AxiosResponse<AcpTemplate[]>> {
+        return this.invokeApi(`/templates`, 'GET');
+    }
+
+    /**
+     * Create template
+     * @param template
+     */
+    async createAcpTemplate(template: AcpTemplate): Promise<AxiosResponse<AcpTemplate>> {
+        return this.invokeApi(`/templates`, 'POST', template);
+    }
+
+    /**
+     * Updates template
+     * @param template
+     */
+    async updateAcpTemplate(template: AcpTemplate): Promise<AxiosResponse<AcpTemplate>> {
+        return this.invokeApi(`/templates`, 'PUT', template);
+    }
+
+    /**
+     * Fetches a template by Id
+     * @param templateId
+     */
+    async fetchAcpTemplateById(templateId: string): Promise<AxiosResponse<AcpTemplate>> {
+        return this.invokeApi(`/templates/${templateId}`, 'GET');
+    }
+
+    /**
+     * Updates template by Id
+     * @param templateId
+     * @param template
+     */
+    async updateAcpTemplateById(templateId: string, template: AcpTemplate): Promise<AxiosResponse<AcpTemplate>> {
+        return this.invokeApi(`/templates/${templateId}`, 'PUT', template);
+    }
+
+    /**
+     * Deletes template
+     * @param templateId
+     */
+    async deleteAcpTemplate(templateId: string): Promise<AxiosResponse> {
+        return this.invokeApi(`/templates/${templateId}`, 'DELETE');
+    }
+
 }
 
 export default new AclGroupServiceRest();
