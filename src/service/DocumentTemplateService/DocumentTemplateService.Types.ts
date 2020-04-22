66db509ef7e9354df84af38927be63aa4ebf87c5
ffjanhoeck <jan.hoeck@flowfact.de>
2020-04-22 18:47:48 +0200

    changes

--- src/service/DocumentTemplateService/DocumentTemplateService.Types.ts
+++ src/service/DocumentTemplateService/DocumentTemplateService.Types.ts
@@ -20,7 +20,7 @@ export interface ReadTemplate extends BaseTemplate {
 }
 
 export interface BaseCategory {
-    parentName?: string;
+    parentName: string | null;
     name: string;
     captions: Captions;
 }
