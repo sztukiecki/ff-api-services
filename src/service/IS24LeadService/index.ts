47a95418733319d52dc12b4fbdd6883fd1483787
Oliver Saternus <oliver.saternus@flowfact.de>
2020-04-27 13:06:24 +0200

    change instance

--- src/service/IS24LeadService/index.ts
+++ src/service/IS24LeadService/index.ts
@@ -4,11 +4,11 @@ export * from './IS24LeadService.Types';
 
 export class IS24LeadService {
 
-    public static instance = new IS24LeadService();
-
     public readonly leads: IS24LeadsController;
 
     constructor() {
         this.leads = new IS24LeadsController();
     }
 }
+
+export const IS24LeadServiceInstance = new IS24LeadService();
