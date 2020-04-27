5f736818dcf55f0e7110facb685951940504cd58
ffjanhoeck <jan.hoeck@flowfact.de>
2020-04-27 18:07:25 +0200

    add saveFormat parameter to generate method

--- src/service/DocumentGeneratorService/GeneratorController.ts
+++ src/service/DocumentGeneratorService/GeneratorController.ts
@@ -13,12 +13,15 @@ export class GeneratorController extends APIClient {
      * @param entities
      * @param userId
      *  The user id for the placeholder-service
+     * @param saveFormat
+     *  undefined means docx
      */
-    generate(s3Url: string, entities: EntityDefinition[], userId?: string) {
+    generate(s3Url: string, entities: EntityDefinition[], userId?: string, saveFormat?: 'pdf' | 'docx') {
         return this.invokeApiWithErrorHandling<{ requestId: string}>('/generator/generate', 'POST', {
             s3Url: s3Url,
             userId: typeof userId === 'string' ? userId : null,
-            entities: entities
+            entities: entities,
+            saveFormat: saveFormat
         });
     }
 
