637421dd407a69f9293272e2abdad5646ad82357
ffjanhoeck <jan.hoeck@flowfact.de>
2020-05-03 12:25:16 +0200

    Add content type

--- src/service/DocumentTemplateService/CategoriesController.ts
+++ src/service/DocumentTemplateService/CategoriesController.ts
@@ -12,7 +12,12 @@ export class CategoriesController extends APIClient {
     }
 
     async createCategory(data: BaseCategory) {
-        return this.invokeApiWithErrorHandling<BaseCategory>('/templates', 'POST', data);
+        return this.invokeApiWithErrorHandling<BaseCategory>('/templates', 'POST', data, {
+            headers: {
+                // We have to set the content type manually here, because otherwise the Content-Type application/problem+json will be set. I don't know why :x
+                'Content-Type': 'application/json'
+            }
+        });
     }
 
     async updateCategory(name: string, data: BaseCategory) {
