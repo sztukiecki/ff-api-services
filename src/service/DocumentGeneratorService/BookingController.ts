c340e5690af68c12ff5af3fc244215aa37e89b49
ffjanhoeck <jan.hoeck@flowfact.de>
2020-05-24 15:56:17 +0200

    DocumentGeneratorService: Book template

---
+++ src/service/DocumentGeneratorService/BookingController.ts
@@ -0,0 +1,18 @@
+import { APIClient, APIMapping } from '../../http';
+
+class BookingController extends APIClient {
+
+    constructor() {
+        super(APIMapping.documentGeneratorService);
+    }
+
+    async bookDocument(requestId: string, entities: ({ entityId: string, schema: string})[]) {
+        return this.invokeApiWithErrorHandling('/booking', 'POST', {
+            requestId,
+            entities
+        });
+    }
+
+}
+
+export default BookingController;
