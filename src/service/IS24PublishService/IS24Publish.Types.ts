5a6f31a3f7069f687e9ed03e2fd23b69b8187324
fftobiashoffmann <tobias.hoffmann@flowfact.de>
2020-08-27 11:31:50 +0200

    Review changes.

--- src/service/IS24PublishService/IS24Publish.Types.ts
+++ src/service/IS24PublishService/IS24Publish.Types.ts
@@ -5,26 +5,28 @@ export namespace IS24PublishTypes {
     }
 
     export interface IS24Statistics {
-        dailyStatistics: [
-            {
-                date: number,
-                exposeHits: number,
-            }
-        ],
+        dailyStatistics: IS24DailyStatistics[]
         externalId: string,
         fromDate: Date,
         toDate: Date,
-        totalStatistics: {
-            clicksHomepage: number,
-            clicksSendUrl: number,
-            emailContacts: number,
-            exposeHits: number,
-            onShortList: number,
-        }
+        totalStatistics: IS24TotalStatistics
     }
 
     export interface IS24Projects {
         channelId: string,
         name: string,
+    }
+
+    export interface IS24TotalStatistics {
+        clicksHomepage: number,
+        clicksSendUrl: number,
+        emailContacts: number,
+        exposeHits: number,
+        onShortList: number,
+    }
+
+    export interface IS24DailyStatistics {
+        date: number,
+        exposeHits: number,
     }
 }
