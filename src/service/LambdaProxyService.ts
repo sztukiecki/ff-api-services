66bd89be83c71a64018f253a5e763262be53be92
Christopher Queiser <christopher.queiser@rising-bits.com>
2020-09-03 11:29:53 +0200

    FLOW-7337 rename with prefix fetch

--- src/service/LambdaProxyService.ts
+++ src/service/LambdaProxyService.ts
@@ -5,11 +5,11 @@ export class LambdaProxyService extends APIClient {
         super(APIMapping.lambdaProxyService);
     }
 
-    async listLogDirectories() {
+    async fetchLogDirectories() {
         return this.invokeApiWithErrorHandling('/ff-importer-v2-record-log-s3-directory-entries', 'GET');
     }
 
-    async getDirectoryContents(table: string) {
+    async fetchDirectoryContents(table: string) {
         return this.invokeApiWithErrorHandling('/ff-importer-v2-record-log-s3-directory-content', 'POST', {table});
     }
 }
