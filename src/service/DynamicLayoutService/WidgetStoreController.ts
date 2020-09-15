95d32fc2d5b556bba971d176c2da674c1c9799d5
Mateusz Fislak <mfislak@valuelogic.one>
2020-09-15 08:03:25 +0200

    Change order of parameters in fetchAssignments

--- src/service/DynamicLayoutService/WidgetStoreController.ts
+++ src/service/DynamicLayoutService/WidgetStoreController.ts
@@ -1,7 +1,7 @@
-import { APIClient, APIMapping } from '../../http';
-import { WidgetTypes } from './WidgetTypes';
+import { APIClient, APIMapping } from "../../http";
+import { WidgetTypes } from "./WidgetTypes";
+import { WidgetLayoutTypes } from "./WidgetLayoutTypes";
 import PagedWidgets = WidgetTypes.PagedWidgets;
-import {WidgetLayoutTypes} from "./WidgetLayoutTypes";
 import LayoutDomainType = WidgetLayoutTypes.LayoutDomainType;
 
 class WidgetStoreController extends APIClient {
@@ -10,7 +10,7 @@ class WidgetStoreController extends APIClient {
         super(APIMapping.dynamicLayoutService);
     }
 
-    async fetchAssignments(layoutDomainType?: LayoutDomainType, schemaName?: string) {
+    async fetchAssignments(schemaName?: string, layoutDomainType: LayoutDomainType = LayoutDomainType.SCHEMA_BOUND) {
         return this.invokeApiWithErrorHandling<PagedWidgets>('/widget-store/widget-assignments', 'GET', undefined, {
             queryParams: {
                 domainType: layoutDomainType,
