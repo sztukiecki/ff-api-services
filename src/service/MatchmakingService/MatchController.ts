41336983f8e1909ee87210c7f740ae8b297f5fe3
Dominik Koller <domkoller@googlemail.com>
2020-09-16 11:51:39 +0200

    FLOW-7832: added map matchmaking calls

--- src/service/MatchmakingService/MatchController.ts
+++ src/service/MatchmakingService/MatchController.ts
@@ -1,61 +1,113 @@
-import { APIClient, APIMapping } from '../../http';
-import { Entity, PagedResponse } from '@flowfact/types';
-import { MatchmakingTypes } from './Matchmaking.Types';
-import { Sort } from '@flowfact/node-flowdsl';
+import { APIClient, APIMapping } from "../../http";
+import { Entity, PagedResponse } from "@flowfact/types";
+import { MatchmakingTypes } from "./Matchmaking.Types";
+import { Sort } from "@flowfact/node-flowdsl";
 
 const buildSortParameter = (sorting: Sort): string | undefined => {
-    // converts sorting to a string like "+fieldName,-fieldName2"
-    return sorting?.fields?.map(fieldName => `${sorting.direction === 'ASC' ? '+' : '-'}${fieldName}`).join(',');
-}
+  // converts sorting to a string like "+fieldName,-fieldName2"
+  return sorting?.fields
+    ?.map(
+      (fieldName) => `${sorting.direction === "ASC" ? "+" : "-"}${fieldName}`
+    )
+    .join(",");
+};
 
 export default class MatchController extends APIClient {
+  constructor() {
+    super(APIMapping.matchmakingService);
+  }
 
-    constructor() {
-        super(APIMapping.matchmakingService);
-    }
+  /**
+   * Fetch matches for a given search profile id
+   * @param searchProfileId
+   * @param query
+   *      The query to filter special results
+   * @param sorting
+   * @param size
+   * @param offset
+   */
+  async fetchMatchesBySearchProfile(
+    searchProfileId: string,
+    query: MatchmakingTypes.FilterQuery = {},
+    sorting: Sort,
+    size: number = 10,
+    offset: number = 0
+  ) {
+    return await this.invokeApiWithErrorHandling<
+      PagedResponse<MatchmakingTypes.Match>
+    >(`/match/search-profile/${searchProfileId}`, "GET", undefined, {
+      queryParams: {
+        ...query,
+        size,
+        offset,
+        sort: buildSortParameter(sorting),
+      },
+    });
+  }
 
-    /**
-     * Fetch matches for a given search profile id
-     * @param searchProfileId
-     * @param query
-     *      The query to filter special results
-     * @param sorting
-     * @param size
-     * @param offset
-     */
-    async fetchMatchesBySearchProfile(searchProfileId: string, query: MatchmakingTypes.FilterQuery = {}, sorting: Sort, size: number = 10, offset: number = 0) {
-        return await this.invokeApiWithErrorHandling<PagedResponse<MatchmakingTypes.Match>>(`/match/search-profile/${searchProfileId}`, 'GET', undefined, {
-            queryParams: { ...query, size, offset, sort: buildSortParameter(sorting) },
-        });
-    }
+  /**
+   * Fetch matches for a given search profile.
+   * @param searchProfile
+   *      A search profile entity
+   * @param query
+   *      The query to filter special results
+   * @param sorting
+   * @param size
+   * @param offset
+   */
+  async fetchMatchesBySearchProfileBody(
+    searchProfile: Entity,
+    query: MatchmakingTypes.FilterQuery = {},
+    sorting: Sort,
+    size: number = 10,
+    offset: number = 0
+  ) {
+    return await this.invokeApiWithErrorHandling<
+      PagedResponse<MatchmakingTypes.Match>
+    >(`/match/search-profile`, "POST", searchProfile, {
+      queryParams: {
+        ...query,
+        size,
+        offset,
+        sort: buildSortParameter(sorting),
+      },
+    });
+  }
 
-    /**
-     * Fetch matches for a given search profile.
-     * @param searchProfile
-     *      A search profile entity
-     * @param query
-     *      The query to filter special results
-     * @param sorting
-     * @param size
-     * @param offset
-     */
-    async fetchMatchesBySearchProfileBody(searchProfile: Entity, query: MatchmakingTypes.FilterQuery = {}, sorting: Sort, size: number = 10, offset: number = 0) {
-        return await this.invokeApiWithErrorHandling<PagedResponse<MatchmakingTypes.Match>>(`/match/search-profile`, 'POST', searchProfile, {
-            queryParams: { ...query, size, offset, sort: buildSortParameter(sorting) },
-        });
-    }
+  /**
+   * Fetch matches for a given estate id.
+   * @param estateId
+   * @param query
+   * @param sorting
+   * @param size
+   * @param offset
+   */
+  async fetchMatchesByEstate(
+    estateId: string,
+    query: MatchmakingTypes.FilterQuery = {},
+    sorting: Sort,
+    size: number = 10,
+    offset: number = 0
+  ) {
+    return await this.invokeApiWithErrorHandling<
+      PagedResponse<MatchmakingTypes.Match>
+    >(`/match/estate/${estateId}`, "GET", undefined, {
+      queryParams: {
+        ...query,
+        size,
+        offset,
+        sort: buildSortParameter(sorting),
+      },
+    });
+  }
 
-    /**
-     * Fetch matches for a given estate id.
-     * @param estateId
-     * @param query
-     * @param sorting
-     * @param size
-     * @param offset
-     */
-    async fetchMatchesByEstate(estateId: string, query: MatchmakingTypes.FilterQuery = {}, sorting: Sort, size: number = 10, offset: number = 0) {
-        return await this.invokeApiWithErrorHandling<PagedResponse<MatchmakingTypes.Match>>(`/match/estate/${estateId}`, 'GET', undefined, {
-            queryParams: { ...query, size, offset, sort: buildSortParameter(sorting) },
-        });
-    }
+  /**
+   * Loads all map matches to the given searchprofile
+   * @param {string} searchProfileId
+   */
+  async fetchMapMatchesBySearchProfile(searchProfileId: string) {
+    return await this.invokeApiWithErrorHandling<
+      PagedResponse<MatchmakingTypes.MapMatch>
+    >(`match/search-profile/${searchProfileId}/map-entries`, "GET");
+  }
 }
--- src/service/MatchmakingService/Matchmaking.Types.ts
+++ src/service/MatchmakingService/Matchmaking.Types.ts
@@ -1,26 +1,44 @@
-import { Entity } from '@flowfact/types';
+import { Entity } from "@flowfact/types";
 
 export namespace MatchmakingTypes {
+  export interface UpdatedField {
+    name: string;
+    previousValue: string | number | object | null;
+    currentValue: string | number | object | null;
+  }
 
-    export interface UpdatedField {
-        name: string;
-        previousValue: string | number | object | null;
-        currentValue: string | number | object | null;
-    }
+  export interface OfferedStatus {
+    alreadyOffered: boolean;
+    offeredAt: number;
+  }
 
-    export interface OfferedStatus {
-        alreadyOffered: boolean;
-        offeredAt: number;
-    }
+  export interface Match {
+    offeredStatus: OfferedStatus;
+    updatedFields: UpdatedField[];
+    entity: Entity;
+  }
 
-    export interface Match {
-        offeredStatus: OfferedStatus;
-        updatedFields: UpdatedField[];
-        entity: Entity;
-    }
+  export interface FilterQuery {
+    ignored?: boolean;
+    offered?: boolean;
+  }
 
-    export interface FilterQuery {
-        ignored?: boolean;
-        offered?: boolean;
-    }
+  export interface MapMatchGeoLocation {
+    latitude: number;
+    longitude: number;
+  }
+
+  export interface MapMatchAddress {
+    street: string;
+    zipcode: string;
+    city: string;
+    geolocation?: MapMatchGeoLocation;
+  }
+
+  export interface MapMatch {
+    id: string;
+    addresses: {
+      values: MapMatchAddress[];
+    };
+  }
 }
