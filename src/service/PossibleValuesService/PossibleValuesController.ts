d2bbdb18335f6c9e1f4133ee5b672f2dd156caa1
Lukas Labeda <lukaslabeda@gmail.com>
2020-09-18 13:14:29 +0200

    FLOW-7357: refactoring

--- src/service/PossibleValuesService/PossibleValuesController.ts
+++ src/service/PossibleValuesService/PossibleValuesController.ts
@@ -1,22 +1,21 @@
 import {APIClient, APIMapping} from '../../http';
-import {PossibleValue} from '@flowfact/types';
+import {PossibleValue} from "@flowfact/types";
 
-export class PossibleValuesService extends APIClient {
+
+export default class PossibleValuesController extends APIClient {
   constructor() {
     super(APIMapping.schemaService);
   }
 
-  getAllPossibleValues = async (schemaName: string) => {
+  async fetchAllPossibleValues(schemaName: string) {
     return this.invokeApiWithErrorHandling<PossibleValue[]>(`/schemas/${schemaName}/possiblevalues`, 'GET', undefined);
   }
 
-  addPossibleValues = async (schemaName: string, possibleValues: any) => {
+  async addPossibleValues(schemaName: string, possibleValues: any) {
     return this.invokeApiWithErrorHandling<void>(`/schemas/${schemaName}/possiblevalues`, 'POST', possibleValues);
   }
 
-  deletePossibleValue = async (schemaName: string, fieldName: string, possibleValue: string) => {
+  async deletePossibleValue(schemaName: string, fieldName: string, possibleValue: string) {
     return this.invokeApiWithErrorHandling<void>(`/schemas/${schemaName}/fields/${fieldName}/possiblevalues/${possibleValue}`, 'DELETE');
   }
 }
-
-export default new PossibleValuesService();
---
+++ src/service/PossibleValuesService/index.ts
@@ -0,0 +1,11 @@
+import PossibleValuesController from './PossibleValuesController';
+
+export class PossibleValuesService {
+  public readonly possibleValues: PossibleValuesController;
+
+  constructor() {
+    this.possibleValues = new PossibleValuesController()
+  }
+}
+
+export const PossibleValuesServiceInstance = new PossibleValuesService();
