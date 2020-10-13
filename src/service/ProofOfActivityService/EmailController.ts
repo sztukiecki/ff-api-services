87bb6ec965adbc8f40f811b222b7d651f2ac96a7
ffjanhoeck <jan.hoeck@flowfact.de>
2020-10-13 09:01:16 +0200

    Change method name

--- src/service/ProofOfActivityService/EmailController.ts
+++ src/service/ProofOfActivityService/EmailController.ts
@@ -20,32 +20,46 @@ export class EmailController extends APIClient {
      * @param entity
      * @param excludedItems
      */
-    async sendEmail(email: Omit<EmailData, 'from'>, dateRange: DateRange, activityId: string, templateId: string, entity: EntityIdData, excludedItems: ExcludedItem[]) {
+    async sendEmail(
+        email: Omit<EmailData, 'from'>,
+        dateRange: DateRange,
+        activityId: string,
+        templateId: string,
+        entity: EntityIdData,
+        excludedItems: ExcludedItem[]
+    ) {
         return this.invokeApiWithErrorHandling('/email/send', 'POST', {
             email,
             dateRange,
             activityId,
             templateId,
             entity,
-            excludedItems
+            excludedItems,
         });
     }
 
-    async preview(email: Omit<EmailData, 'from'>, dateRange: DateRange, activityId: string, templateId: string, entity: EntityIdData, excludedItems: ExcludedItem[]) {
+    async generatePreview(
+        email: Omit<EmailData, 'from'>,
+        dateRange: DateRange,
+        activityId: string,
+        templateId: string,
+        entity: EntityIdData,
+        excludedItems: ExcludedItem[]
+    ) {
         const body = {
             email,
             dateRange,
             activityId,
             templateId,
             entity,
-            excludedItems
+            excludedItems,
         };
 
         return this.invokeApiWithErrorHandling<string>('/email/preview', 'POST', body, {
             headers: {
                 'Content-Type': 'application/json',
-                Accept: 'text/html'
-            }
+                Accept: 'text/html',
+            },
         });
     }
 }
