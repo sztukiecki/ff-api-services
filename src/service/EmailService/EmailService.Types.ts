34b605d9922bee76ee1f93e6771ee1106cbabba5
Dominik Koller <domkoller@googlemail.com>
2020-11-10 15:18:02 +0100

    FLOW-8365: reworked emailservice, added notify

---
+++ src/service/EmailService/EmailService.Types.ts
@@ -0,0 +1,38 @@
+export namespace EmailServiceTypes {
+    export interface DnsEntry {
+        valid: boolean;
+        type: 'a' | 'cname';
+        host: string;
+        data: string;
+    }
+    export interface DomainConfigurationResponse {
+        domain: string;
+        valid: boolean;
+        dnsEntries: DnsEntry[];
+    }
+    export interface Email {
+        mailFrom: string;
+        replyTo: string;
+        recipientList: string[];
+        blindCopyList?: string[];
+        carbonCopyList?: string[];
+        subject: string;
+        body: string;
+        schemaId?: string;
+        entityId?: string;
+    }
+
+    export enum EmailTemplate {
+        AdviceadviceInteractiveExpose = 'adviceInteractiveExpose',
+        AdviceEmail = 'adviceEmail',
+        AdviceInquiryProcessing = 'adviceInquiryProcessing',
+        AdvicePortalInsertion = 'advicePortalInsertion',
+    }
+
+    export enum EmailRecipient {
+        ACCOUNT_MANAGER = 'ACCOUNT_MANAGER',
+        ADMIN = 'ADMIN',
+        USER = 'USER',
+        EXTERNAL = 'EXTERNAL',
+    }
+}
