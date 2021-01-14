95955cbe68bdf8f3b5f660c90cc62451252f14fe
dpotera <dominik.potera@gmail.com>
2021-01-14 15:05:46 +0100

    FLOW-9428 Create SmsIngestionService

---
+++ src/service/SmsIngestionService/SmsIngestionService.Types.ts
@@ -0,0 +1,10 @@
+
+export namespace SmsIngestionServiceTypes {
+    export interface SmsData {
+        estateId?: string;
+        senderPhoneNumber: string;
+        recipients: string[];
+        recipientsPhoneNumber: string[];
+        message: string;
+    }
+}
