ee99e8280d4e7dd9d390273d8a5cccf4b878878f
Sajed <sizmamani@yahoo.com>
2021-01-14 10:51:51 +0100

    FLOW-9228: add detailedResult

--- src/service/PreconditionService/PreconditionsService.Types.ts
+++ src/service/PreconditionService/PreconditionsService.Types.ts
@@ -1,5 +1,13 @@
 export namespace PreconditionServiceTypes {
+    export interface DetailedResult {
+        companyBaseData: boolean;
+        companyLegalData: boolean;
+        companyLegislationTexts: boolean;
+        iexSettings: boolean;
+        user: boolean;
+    }
     export interface Precondition {
         satisfied: boolean;
+        detailedResult: DetailedResult;
     }
 }
