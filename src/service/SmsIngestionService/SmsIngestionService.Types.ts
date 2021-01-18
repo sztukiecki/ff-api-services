437ed07255fcfba67ecaf14aa466fb411e74d87b
dpotera <dominik.potera@gmail.com>
2021-01-18 15:25:22 +0100

    FLOW-9607 add fetchCredentials call to SmsIngestionService

--- src/service/SmsIngestionService/SmsIngestionService.Types.ts
+++ src/service/SmsIngestionService/SmsIngestionService.Types.ts
@@ -6,4 +6,11 @@ export namespace SmsIngestionServiceTypes {
         recipientsPhoneNumber: string[];
         message: string;
     }
+
+    export interface Credentials {
+        companyId: string;
+        token: string;
+        created: Date;
+        lastUpdated: Date;
+    }
 }
