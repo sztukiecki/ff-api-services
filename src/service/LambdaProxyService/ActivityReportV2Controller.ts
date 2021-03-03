f1ead75e38af0469087ad19bd6dc1a3c6c24439e
Viet Pham <viet.pham@flowfact.de>
2021-03-03 12:39:03 +0100

    Remove public keyword

--- src/service/LambdaProxyService/ActivityReportV2Controller.ts
+++ src/service/LambdaProxyService/ActivityReportV2Controller.ts
@@ -8,6 +8,32 @@ export class ActivityReportV2Controller extends APIClient {
         super(APIMapping.lambdaProxyService);
     }
 
+    /**
+     * generate activity report from estate
+     * @param entityId entity id of the estate object
+     */
+    async generateActivityReportV2(entityId: string): Promise<ApiResponse<LambdaServiceResponse>> {
+        return this.invokeActivityReportV2('generate', entityId);
+    }
+
+    /**
+     * publish activity report data as context file in the preconfigured AWS S3 bucket
+     * @param entityId entity id of the activity report object
+     */
+    async publicActivityReportV2(entityId: string): Promise<ApiResponse<LambdaServiceResponse>> {
+        return this.invokeActivityReportV2('publish', entityId);
+    }
+
+    /**
+     * Generate a URL to get a preview of activity report, without creating a context file
+     * @param activityReportId entity id of the activity report instance
+     */
+    async generateActivityReportPreviewUrl(activityReportId: string): Promise<string> {
+        const baseUrl = EnvironmentManagementInstance.getActivityReportUrl();
+        const authenticationToken = await this.getAuthenticationToken();
+        return `${baseUrl}/preview?hash=${authenticationToken}&id=${activityReportId}`;
+    }
+
     private async prepareActivityReportV2Body(method: ActivityReportRequestMethod, entityId: string): Promise<ActivityReportRequestBody> {
         const authenticationToken = await this.getAuthenticationToken();
         return {
@@ -20,31 +46,5 @@ export class ActivityReportV2Controller extends APIClient {
     private async invokeActivityReportV2(method: ActivityReportRequestMethod, entityId: string): Promise<ApiResponse<LambdaServiceResponse>> {
         const body = await this.prepareActivityReportV2Body(method, entityId);
         return await this.invokeApiWithErrorHandling<LambdaServiceResponse>('/activity-report2-lambda', 'POST', body, {headers: {'Content-Type': 'application/json'}});
-    }
-
-    /**
-     * generate activity report from estate
-     * @param entityId entity id of the estate object
-     */
-    public async generateActivityReportV2(entityId: string): Promise<ApiResponse<LambdaServiceResponse>> {
-        return this.invokeActivityReportV2('generate', entityId);
-    }
-
-    /**
-     * publish activity report data as context file in the preconfigured AWS S3 bucket
-     * @param entityId entity id of the activity report object
-     */
-    public async publicActivityReportV2(entityId: string): Promise<ApiResponse<LambdaServiceResponse>> {
-        return this.invokeActivityReportV2('publish', entityId);
-    }
-
-    /**
-     * Generate a URL to get a preview of activity report, without creating a context file
-     * @param activityReportId entity id of the activity report instance
-     */
-    public async generateActivityReportPreviewUrl(activityReportId: string): Promise<string> {
-        const baseUrl = EnvironmentManagementInstance.getActivityReportUrl();
-        const authenticationToken = await this.getAuthenticationToken();
-        return `${baseUrl}/preview?hash=${authenticationToken}&id=${activityReportId}`;
     }
 }
