e962f42a45a168c23a1974849b555ed160bcfb3c
Radek Wróblewski <rwroblewski@valuelogic.one>
2021-03-23 10:56:49 +0100

    feat CXGFF-12 - IMMO 2nd placement - new endpoint; extract immo endpoints to separate controller

---
+++ src/service/IS24ImportService/IS24ImmoController.ts
@@ -0,0 +1,61 @@
+import {APIClient, APIMapping} from "../../http";
+import {IS24ImportServiceTypes} from './IS24ImportService.Types';
+import ImmoResponse = IS24ImportServiceTypes.ImmoResponse;
+import ImmoAvailabilityInfo = IS24ImportServiceTypes.ImmoAvailabilityInfo;
+
+export default class IS24ImmoController extends APIClient {
+    constructor() {
+        super(APIMapping.is24ImportService);
+    }
+
+    /**
+     * Fetches IMMO widget URL for given estate
+     * @param portalId
+     * @param entityId
+     * @param returnUrl
+     */
+    async fetchImmoWidgetUrl(portalId: string, entityId: string, returnUrl: string) {
+        return await this.invokeApiWithErrorHandling<ImmoResponse>(
+            `/portal/${portalId}/estate/${entityId}/immo`,
+            'GET',
+            undefined,
+            {
+                queryParams: {
+                    returnUrl,
+                },
+            });
+    }
+
+    /**
+     * Fetches IMMO widget availability info for given estate
+     * @param entityId
+     */
+    async fetchImmoWidgetEstateAvailabilityInfo(entityId: string) {
+        return await this.invokeApiWithErrorHandling<ImmoAvailabilityInfo>(
+            `/estate/${entityId}/immoAvailability`,
+            'GET');
+    }
+
+    /**
+     * Fetches IMMO widget availability info for given portal and estate
+     * @param portalId
+     * @param entityId
+     */
+    async fetchImmoWidgetEstateAvailabilityInfoForPortal(portalId: string, entityId: string) {
+        return await this.invokeApiWithErrorHandling<ImmoAvailabilityInfo>(
+            `/portal/${portalId}/estate/${entityId}/immoAvailability`,
+            'GET');
+    }
+
+
+    /**
+     * Checks if IMMO widget is available for given portal without estate context
+     * @param portalId
+     */
+    async isImmoWidgetAvailableForPortal(portalId: string) {
+        return await this.invokeApiWithErrorHandling<Boolean>(
+            `/portal/${portalId}/immoAvailability`,
+            'GET');
+
+    }
+}
