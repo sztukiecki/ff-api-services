6fefc697d03e474b74a7dff4d1b40ca7ed37cd55
Sajed <mostafa.shariati@scout24.com>
2021-03-25 11:43:02 +0100

    FLOW-10713: add geo information

--- src/service/GeoInformationService/SearchController.ts
+++ src/service/GeoInformationService/SearchController.ts
@@ -1,7 +1,7 @@
 import { APIClient, APIMapping } from '../../http';
 import {GeoInformationsServiceTypes} from './GeoInformationService.Types';
 import FindPolygonResponse = GeoInformationsServiceTypes.FindPolygonResponse;
-
+import GeoInformationValue = GeoInformationsServiceTypes.GeoInformationValue;
 
 export class SearchController extends APIClient {
     constructor() {
@@ -15,4 +15,21 @@ export class SearchController extends APIClient {
     async findPolygon(query: string) {
         return this.invokeApiWithErrorHandling<FindPolygonResponse>(`/polygons/search?q=${query}`, 'GET');
     }
+
+    /**
+     * fetch one polygon by caption
+     * @param caption
+     */
+     async fetchPolygonDetail(caption: string) {
+        return this.invokeApiWithErrorHandling<GeoInformationValue>(`/polygons/${caption}`, 'GET');
+    }
+    
+    /**
+     * fetch polygons list by captions
+     * @param captions
+     */
+     async fetchPolygonDetails(captions: string) {
+        return this.invokeApiWithErrorHandling<GeoInformationValue[]>(`/polygons/list?names=${captions}`, 'GET');
+    }
+
 }
